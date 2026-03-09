2603ed431e3f11af5cbbf77cbd2896f3f419237a
camillejoannes13-del <camillejoannes13@gmail.com>
2026-03-09 09:25:19 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(67)
+    music.play(music.stringPlayable("C5 D A F G E B C ", 383), music.PlaybackMode.UntilDone)
+})
+basic.forever(function () {
+	
+})
